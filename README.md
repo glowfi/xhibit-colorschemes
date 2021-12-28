b4764d69e51c9989546f0bd363f45dee64ea78a5
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-28 23:28:27 +0530

    added colorshemes

--- README.md
+++ README.md
@@ -3,12 +3,17 @@
 Colorschemes for xhibit library
 
 ### Try all colorscheme in 1 second interval
+
 ```
 wget https://raw.githubusercontent.com/glowfi/xhibit-colorschemes/main/colorscheme.txt -O ~/.cache/colsx.txt
+git clone https://github.com/glowfi/xhibit-colorschemes
+mv ~/xhibit-colorschemes/themes ~
+rm -rf xhibit-colorschemes
+
 input=~/.cache/colsx.txt
 while IFS= read -r line; do
 	echo "$line"
-	output=$(cat ~/th/"$line" | xargs | tr " " ",")
+	output=$(cat ~/themes/"$line" | xargs | tr " " ",")
 	xhibit -ccs "$output" -rcn t
 	sleep 1
 done <"$input"
--- script.sh
+++ script.sh
@@ -1,35 +1,32 @@
-#!/bin/bash
-# input=~/a.txt
-# while IFS= read -r line; do
-# 	out=$(cat ~/themes/"$line" | awk '/bright/,0')
-# 	touch ~/th/"$line"
-# 	echo "$out" >>~/th/"$line"
-# done <"$input"
+#!/bin/sh
 
-# input=~/a.txt
-# while IFS= read -r line; do
-# sed -i 's/bright://g' ~/th/"$line"
-# sed -i 's/black://g' ~/th/"$line"
-# sed -i 's/red://g' ~/th/"$line"
-# sed -i 's/green://g' ~/th/"$line"
-# sed -i 's/yellow://g' ~/th/"$line"
-# sed -i 's/blue://g' ~/th/"$line"
-# sed -i 's/magenta://g' ~/th/"$line"
-# sed -i 's/cyan://g' ~/th/"$line"
-# sed -i 's/white://g' ~/th/"$line"
-# sed -i '/^$/d;s/[[:blank:]]//g' ~/th/"$line"
-# sed -i -e "1d" ~/th/"$line"
-# exit
-# 	out=$(cat ~/themes/"$line" | awk '/bright/,0')
-# 	touch ~/th/"$line"
-# 	echo "$out" >>~/th/"$line"
-# done <"$input"
+# DOWNLOAD
+git clone https://github.com/rajasegar/alacritty-themes
+mv ~/alacritty-themes/themes ~
+mkdir ~/th
 
+# EXTRACT
 input=~/a.txt
 while IFS= read -r line; do
-	echo "$line"
-	output=$(cat ~/th/"$line" | xargs | tr " " ",")
-	xhibit -ccs "$output" -rcn t
-	sleep 1
-	# exit
+	out=$(cat ~/themes/"$line" | awk '/bright/,0')
+	touch ~/th/"$line"
+	echo "$out" >>~/th/"$line"
 done <"$input"
+
+# SANITIZE
+input=~/a.txt
+while IFS= read -r line; do
+	sed -i 's/bright://g' ~/th/"$line"
+	sed -i 's/black://g' ~/th/"$line"
+	sed -i 's/red://g' ~/th/"$line"
+	sed -i 's/green://g' ~/th/"$line"
+	sed -i 's/yellow://g' ~/th/"$line"
+	sed -i 's/blue://g' ~/th/"$line"
+	sed -i 's/magenta://g' ~/th/"$line"
+	sed -i 's/cyan://g' ~/th/"$line"
+	sed -i 's/white://g' ~/th/"$line"
+	sed -i '/^$/d;s/[[:blank:]]//g' ~/th/"$line"
+	sed -i -e "1d" ~/th/"$line"
+done <"$input"
+
+rm -rf alacritty-themes
